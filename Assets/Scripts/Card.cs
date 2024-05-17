3ee8165791f62c1934f3ea3443a3034f67e7555b
subak222 <sbag88686@gmail.com>
2024-05-17 11:00:44 +0900

    init

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class Card : MonoBehaviour
+{
+    Animator anim;
+    public int decknum;
+
+    private float lastAnimTime = 0;
+
+    CardManager cardManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        cardManager = GameObject.Find("CardManager").GetComponent<CardManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(cardManager.show);
+        if (Input.GetMouseButtonDown(0) && cardManager.show == true)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray);
+
+            if (hit.collider != null)
+            {
+                if (hit.collider.gameObject.name == "card"+decknum.ToString())
+                {
+                    anim.SetInteger("show", decknum);
+                    cardManager.cardCount--;
+                    cardManager.anim.SetInteger("getCard", cardManager.cardCount);
+                    cardManager.show = false;
+                    gameObject.GetComponent<BoxCollider2D>().enabled = false;
+                }
+            }
+        }
+        float animTime = anim.GetCurrentAnimatorStateInfo(0).normalizedTime;
+        if (animTime >= 1.0f && lastAnimTime < 1.0f && anim.GetCurrentAnimatorStateInfo(0).IsName("PutCard"))
+        {
+            gameObject.GetComponent<BoxCollider2D>().enabled = true;
+            lastAnimTime = 1.0f;
+            anim.SetInteger("show", 0);
+            cardManager.myCardDeck[cardManager.cardCount-1].mycard.SetActive(false);
+            for (int i = decknum-1; i < 20; i++)
+            {
+                cardManager.myCardDeck[i].cardNumber = cardManager.myCardDeck[i + 1].cardNumber;
+                cardManager.myCardDeck[i].mycard.GetComponent<SpriteRenderer>().sprite = cardManager.myCardDeck[i + 1].mycard.GetComponent<SpriteRenderer>().sprite;
+            }
+            cardManager.show = true;
+        }
+        else
+        {
+            lastAnimTime = animTime;
+        }
+    }
+}
---
+++ Assets/Scripts/CardManager.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CardManager : MonoBehaviour
+{
+    [Serializable]
+    public struct Card
+    {
+        public int cardNumber;
+        public GameObject mycard;
+    }
+
+    [SerializeField]
+    public Animator anim;
+    [SerializeField]
+    private Sprite[] cardSprites;
+    public Card[] myCardDeck;
+
+    public int cardCount = 1;
+    public bool show = true;
+    private float lastAnimTime = 0f;
+
+    public List<int> array = new List<int>();
+
+    void Start()
+    {
+        anim.SetInteger("getCard", cardCount);
+        anim.SetBool("nextAnim", false);
+        for (int i = 0; i < 54; i++) // Assuming there are 54 cards
+        {
+            array.Add(i);
+        }
+
+        Shuffle(array);
+    }
+
+    void Shuffle(List<int> array)
+    {
+        System.Random random = new System.Random();
+        for (int i = array.Count - 1; i > 0; i--)
+        {
+            int j = random.Next(0, i + 1); // 0부터 i까지의 랜덤 인덱스 선택
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray);
+
+            if (hit.collider != null)
+            {
+                if (hit.collider.gameObject.layer == LayerMask.NameToLayer("getCard"))
+                {
+                    StartCoroutine(MyGetCard());
+                }
+            }
+        }
+        float animTime = anim.GetCurrentAnimatorStateInfo(0).normalizedTime;
+        if (animTime >= 1.0f && lastAnimTime < 1.0f)
+        {
+            lastAnimTime = 1.0f;
+            anim.SetBool("nextAnim", false);
+            int deck = array[0];
+            array.RemoveAt(0);
+            myCardDeck[cardCount - 1].mycard.GetComponent<SpriteRenderer>().sprite = cardSprites[deck];
+            myCardDeck[cardCount - 1].cardNumber = deck;
+            myCardDeck[cardCount - 1].mycard.SetActive(true);
+            cardCount++;
+            anim.SetInteger("getCard", cardCount);
+        }
+        else
+        {
+            lastAnimTime = animTime;
+        }
+
+    }
+
+    IEnumerator MyGetCard()
+    {
+        anim.SetBool("nextAnim", true);
+        yield return new WaitForSeconds(0.1f);
+    }
+}
